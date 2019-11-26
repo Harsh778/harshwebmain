b237db0fc4588cc471a112496b2657eb42b9cab4
Harsh778 <51576476+Harsh778@users.noreply.github.com>
2019-11-27 00:33:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-$("#switch").on("click", () => {
-  if($("#switch").prop("checked")) {
-      $(".wrapper").addClass("dark");
-  } else {
-    $(".wrapper").removeClass("dark");
-  }
-})
+var nightModeToggleButton = document.querySelector(".nightModeButton");
+var container = document.querySelector(".container");
+var para = document.querySelector(".paragraph");
+var header = document.querySelector(".header");
+var body = document.querySelector("body");
+
+nightModeToggleButton.onclick = function () {
+  nightModeToggleButton.classList.toggle("night-mode");
+  container.classList.toggle("night-mode");
+  para.classList.toggle("night-mode");
+  header.classList.toggle("night-mode");
+  body.classList.toggle("night-mode");
+};
