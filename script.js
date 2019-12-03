e79b6af6334f9adc902fdb5de838c13dd60a9233
Harsh778 <51576476+Harsh778@users.noreply.github.com>
2019-12-03 23:48:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
     function dark() {
         if (document.body.style.backgroundColor == 'rgb(255, 255, 255)') {
 
-                document.body.style.backgroundColor = '#333';
+                document.body.style.backgroundColor = '#00008b';
         }
         else {
                 document.body.style.backgroundColor = 'rgb(255, 255, 255)';
