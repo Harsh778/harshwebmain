fed57ef255fbba295e335560f0913ad7fad85fd3
Harsh Vardhan Singhal <51576476+Harsh778@users.noreply.github.com>
2020-02-11 12:13:30 +0530

    Update darkmode

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
     function dark() {
         if (document.body.style.backgroundColor == 'rgb(255, 255, 255)') {
 
-                document.body.style.backgroundColor = '#333';
+                document.body.style.backgroundColor = '#03001f';
         }
         else {
                 document.body.style.backgroundColor = 'rgb(255, 255, 255)';
