dfe0f36395fd2a0519ed7afa5fafb47780957fe9
Harsh Vardhan Singhal <51576476+harsh778@users.noreply.github.com>
2020-05-12 20:52:38 +0530

    Add redirect to V2.0 upon loading

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-    function dark() {
+window.onload = function() {
+    window.location.href = "http://harsh778.github.io/harshsinghal.com";
+}    
+
+
+function dark() {
         if (document.body.style.backgroundColor == 'rgb(255, 255, 255)') {
 
                 document.body.style.backgroundColor = '#03001f';
