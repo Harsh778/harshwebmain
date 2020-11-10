33a4f059571e687864c43242aa745bc41975cac5
Harsh Vardhan Singhal <51576476+harsh778@users.noreply.github.com>
2020-11-10 14:23:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-window.onload = function() {
-    window.setTimeout('window.open("https://harshsinghal.me","newsite")',3000);
-
-    window.location.href = "http://harshsinghal.me";
-}    
-
-
 function dark() {
         if (document.body.style.backgroundColor == 'rgb(255, 255, 255)') {
 
